8aa478fdc48dff660715919b9a3f37a14688b3a2
Bruno Fanini <phoenixbf@gmail.com>
2025-10-19 23:30:34 +0200

    radial techniques

---
+++ js/item.js
@@ -0,0 +1,79 @@
+/*
+
+    IlluminAI SUI Item
+ 
+=============================*/
+
+class Item extends ATON.Node {
+
+constructor(id, db){
+    super(id, ATON.NTYPES.UI);
+
+    this._id = id;
+
+    if (!db) return;
+    this.data = APP.db[db][id];
+
+    this.panel = new ATON.SUI.MediaPanel("panel-"+id);
+    this.panel.setTitle( id );
+    //this.panel.setBackdrop(0.5);
+
+    this.panel.attachTo(this);
+    this.panel.enablePicking();
+
+    this.enablePicking();
+    this.setupEvents();
+
+    this._bIspection = false;
+
+}
+
+setupEvents(){
+    this.panel.onHover = ()=>{
+        if (this._bIspection) return;
+
+        ///this.setBackgroundOpacity(1.0);
+        this.setScale(APP.ITEM_SCALE * 1.2);
+        //this.position.x *= 0.9;
+        //this.position.z *= 0.9;
+
+        ATON.AudioHub.playOnceGlobally(ATON.PATH_RES+"audio/blop.mp3");
+        console.log(this._id);
+    };
+
+    this.panel.onLeave = ()=>{
+        if (this._bIspection) return;
+
+        //this.setBackgroundOpacity(0.5);
+        this.setScale(APP.ITEM_SCALE);
+    };
+
+    this.panel.onSelect = ()=>{
+        if (!this._bIspection){
+            this._bIspection = true;
+            
+            this.load(4096);
+
+            this.position.x = THREE.MathUtils.lerp(this.position.x,this._origin.x, 0.5);
+            this.position.z = THREE.MathUtils.lerp(this.position.z,this._origin.z, 0.5);
+
+            this.setScale(APP.ITEM_SCALE * 2.0);
+        }
+    };
+}
+
+load(res){
+    if (!res) res = 4096;
+
+    this.panel.load( APP.getImageURL(this.data.path, res) );
+    
+    this.enablePicking();
+}
+
+setClusterOrigin(p){
+    this._origin = p;
+}
+
+}
+
+export default Item;
--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
+/*
+    IlluminAI app
+
+==========================================*/
+import Item from "./item.js";
 
 
 let APP = ATON.App.realize();
 //APP.requireFlares(["myFlare"]);
 window.APP = APP;
 
+APP.Item = Item;
+
+APP.bgColor = new THREE.Color(0.1,0.1,0.1);
 APP.pathConfigFile   = APP.basePath + "config.json";
 APP.pathResAssets    = APP.basePath + "assets/";
 
-APP.confdata = undefined;
+APP.ITEM_SCALE = 0.25;
 
+APP.confdata  = undefined;
+APP.cloudbase = undefined;
+APP.db        = {};
 
 
 APP.loadConfig = ()=>{
@@ -17,6 +28,25 @@ APP.loadConfig = ()=>{
         console.log("Loaded config");
 
         APP.confdata = data;
+        APP.cloudbase = data.ncdata;
+
+        if (data.db){
+            let numCSVs = Object.keys(data.db).length;
+            let dbloaded=0;
+
+            for (let e in data.db){
+                const E = data.db[e];
+
+                if (!APP.db[e]) APP.db[e] = {};
+
+                ATON.ASCII.loadCSV(APP.basePath +"db/"+E, 2, (d)=>{
+                    APP.db[e] = d;
+                    dbloaded++;
+
+                    if (dbloaded>= numCSVs) ATON.fire("APP_DB_READY");
+                }); 
+            }
+        }
 
         ATON.fire("APP_ConfigLoaded");
     });
@@ -27,15 +57,86 @@ APP.setup = ()=>{
     ATON.realize();
     ATON.UI.addBasicEvents();
 
+    ATON.Nav.setAndRequestHomePOV(
+        new ATON.POV().setPosition(-1,1.5,0).setTarget(0,1.5,0)
+    );
+
+    APP.setupScene();
+
     APP.loadConfig();
 
-    ATON.ASCII.loadCSV(APP.basePath+"db/PoA_Ver5.1_Gruppo_40k_predicted_labels_v1.0_CorrectedwithAnnotations.csv", 5, (data)=>{
-        console.log(data);
-    });
+    APP.setupEvents();
 };
 
-// Run the App
-window.addEventListener('load', ()=>{
-	APP.run();
-});
+APP.setupScene = ()=>{
+    ATON.setBackgroundColor(APP.bgColor);
+    ATON._bqScene = true;
 
+    // Base Scene
+    let g = new THREE.PlaneGeometry( 4, 4 );
+    let matBase = new THREE.MeshStandardMaterial({
+        transparent: true,
+        side: THREE.DoubleSide,
+        depthWrite: false,
+    });
+
+    ATON.Utils.textureLoader.load(APP.pathResAssets+"base2.png", (tex)=>{
+        tex.colorSpace = ATON._stdEncoding;
+        matBase.map = tex;
+
+        matBase.needsUpdate = true;
+    });
+
+    let mBase = new THREE.Mesh(g,matBase);
+    mBase.rotateX(-Math.PI * 0.5);
+
+    let nBase = ATON.createSceneNode();
+    nBase.add(mBase);
+    nBase.enablePicking().attachToRoot();
+};
+
+APP.setupEvents = ()=>{
+    ATON.on("APP_DB_READY", APP.onReadyDB );
+};
+
+APP.onReadyDB = ()=>{
+    console.log("DB ready")
+
+    // Cluster
+    let C = ATON.createUINode("C0");
+    C.attachToRoot();
+
+    let count = 0;
+    for (let e in APP.db["main"]){
+
+        if (count < 50){
+            let P = new APP.Item(e, "main");
+            P.setClusterOrigin(C.position);
+
+            P.attachTo(C);
+            P.load(128);
+
+            count++;
+        }
+    }
+
+    let spiral = (N,i)=>{
+        let y = 0.3 + (i * 0.03);
+
+        let x = 2.0 * Math.cos(i*0.3);
+        let z = 2.0 * Math.sin(i*0.3);
+
+        N.setPosition(x,y,z).orientToLocation(0,y,0).setScale(APP.ITEM_SCALE);
+    };
+
+    ATON.SUI.createLayout(C, spiral);
+};
+
+// https://<nccloud-addr>/apps/files_sharing/publicpreview/iq2623RQzQbe33E?x=1024&y=1024&a=true&file=/A_Art/A.01_Painting/A.01_A_18GRmGFN-B000237.jpg
+APP.getImageURL = (path, res)=>{
+    if (!APP.cloudbase) return undefined;
+
+    if (!res) res = 2048;
+
+    return APP.cloudbase+"?x="+res+"&y="+res+"&a=true&file=/"+path;
+};
